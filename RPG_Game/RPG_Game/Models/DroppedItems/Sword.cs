a40a60351853b4b134c337e7b2cab347f2b1a3e2
BarishYumerov <barish.yumerov@abv.bg>
2015-02-16 23:15:04 +0200

    Fixed constructor of dropped items

--- RPG_Game/RPG_Game/Models/DroppedItems/Sword.cs
+++ RPG_Game/RPG_Game/Models/DroppedItems/Sword.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using RPG_Game.Interfaces;
+using RPG_Game.Models;
+using RPG_Game.Enums;
+using RPG_Game.GameData;
 
 namespace RPG_Game.Models.DroppedItems
 {
-    class Sword
+    class Sword : GameItem, IWearable
     {
+        public Sword(int attackBonuc, int defenceBonus, int healthBonus, int manaBonus, Flags flags,
+            Texture2D texture, IMap map, Point position, string name)
+            : base(texture, map, position, name)
+        {
+            this.AttackBonus = attackBonuc;
+            this.DefenceBonus = defenceBonus;
+            this.HealthBonus = healthBonus;
+            this.ManaBonus = manaBonus;
+            this.Flags = Flags.IsItem;
+        }
+
+        public int AttackBonus { get; set; }
+
+        public int DefenceBonus { get; set; }
+
+        public int HealthBonus { get; set; }
+
+        public int ManaBonus { get; set; }
     }
 }
